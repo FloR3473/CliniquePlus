80c988180d43a21e369f7fc60565084f40067890
Lola Erhart <erhart.lola@hotmail.fr>
2026-06-12 16:29:31 +0200

    <Feat> Ajout des tables patients et medecins dans la Bdd

--- initBdd.js
+++ initBdd.js
@@ -26,6 +26,66 @@ db.serialize(() => {
     });
 
     db.run(`
+        CREATE TABLE IF NOT EXISTS patients (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            mail TEXT NOT NULL UNIQUE,
+            nom TEXT NOT NULL,
+            prenom TEXT NOT NULL,
+            NSS TEXT NOT NULL UNIQUE
+        )
+    `, (err) => {
+        if (err) {
+            console.error("Erreur création table :", err.message);
+        } else {
+            console.log("Table patients prête");
+        }
+    });
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS medecins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nom TEXT NOT NULL,
+            prenom TEXT NOT NULL,
+            specialite TEXT NOT NULL
+        )
+    `, (err) => {
+        if (err) {
+            console.error("Erreur création table :", err.message);
+        } else {
+            console.log("Table medecins prête");
+        }
+    });
+
+
+    db.run(`
+        INSERT OR IGNORE INTO medecins  (nom, prenom, specialite)
+        VALUES (?, ?, ?)
+    `, ["Dupont", "Roger", "Cardiologue"], function (err) {
+        if (err) {
+            console.error("Erreur insertion médecins :", err.message);
+        } else if (this.changes === 0) {
+            console.log("Médecin existant");
+        } else {
+            console.log("Médecin créé");
+        }
+    });
+
+
+    db.run(`
+        INSERT OR IGNORE INTO patients (mail, nom, prenom, NSS)
+        VALUES (?, ?, ?)
+    `, ["Celeste@hotmail.fr", "Martin", "Céleste", "890120505"], function (err) {
+        if (err) {
+            console.error("Erreur insertion patients :", err.message);
+        } else if (this.changes === 0) {
+            console.log("Patients toujours vivant");
+        } else {
+            console.log("Patients créé");
+        }
+    });
+
+
+    db.run(`
         INSERT OR IGNORE INTO users (mail, password, role)
         VALUES (?, ?, ?)
     `, ["admin@cliniqueplus.fr", "azerty", "admin"], function (err) {
